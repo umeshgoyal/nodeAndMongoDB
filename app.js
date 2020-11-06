e7b77b107e0e6a5c13c9954df4733d365bcf726a
archit4077 <architgupta@criodo.com>
2020-11-06 14:11:31 +0530

    Added middleware for date capture

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const captureDate = require('./middleware/middleware');
 
 mongoose.connect("mongodb://localhost:27017/TODOAPP",{useCreateIndex:true,useUnifiedTopology:true, useNewUrlParser:true});
+
+app.use(captureDate);
 app.use(bodyParser.urlencoded({
         extended: true
 }));
---
+++ middleware/middleware.js
@@ -0,0 +1,7 @@
+const captureDate = (req, res, next) => {
+    res.locals.dateCreated = new Date();
+    console.log(`URL:  ${req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`);
+    next();
+}
+
+module.exports = captureDate;
