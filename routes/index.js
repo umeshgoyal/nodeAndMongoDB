655163af2b10ecee82d4814d4b41093e28bfdfed
Aman Kumar Kesu <aman.kesu@crio-users.in>
2020-11-18 18:03:08 +0530

    [Added querying for last and next days ]

--- routes/index.js
+++ routes/index.js
@@ -14,20 +14,94 @@ router.get("/todos", (req,res) => {
         }
         else{
             console.log(allTodos);
-            res.send(allTodos);
+            res.json(allTodos);
+           // res.send(allTodos);
         }
     });
 });
 
 
+/* Get all TODOS: Which are pending   
+curl http://localhost:3000/todos/pending
+*/
+router.get("/todos/pending", (req,res) => {
+    Todos.find({pending:true}, (err,allTodos) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(allTodos);
+            res.json(allTodos);
+           // res.send(allTodos);
+        }
+    });
+});
+
+
+/* Get all TODOS: :Last x days( EG: 15days )  
+curl http://localhost:3000/todos/endDate
+*/
+router.get("/todos/endDate", (req,res) => {
+    var lastday= new Date();
+    lastday.setTime(lastday.getTime() - (15*24*60*60*1000));
+    var today= new Date();
+    today.setTime(today.getTime() );
+    Todos.find({
+        startDate:{
+            $lte:today, $gte:lastday
+        }
+    },
+     (err,allTodos) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(allTodos);
+            res.json(allTodos);
+           // res.send(allTodos);
+        }
+    });
+});
+
+
+
+/* Get all Todos : Next x days (EG: 15 days)
+curl -X "GET" http://localhost:3000/todos/startDate
+*/
+
+router.get("/todos/startDate",(req,res)=>{
+    var nextday= new Date();
+    nextday.setTime(nextday.getTime() + (15*24*60*60*1000));
+    var today= new Date();
+    today.setTime(today.getTime() );
+    Todos.find({
+        startDate:{
+            $lte:nextday, $gte:today
+        }
+    },
+    (err,allTodos) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(allTodos);
+            res.json(allTodos);
+           // res.send(allTodos);
+        }
+    });
+});
+
 /* Add a TODO to the list
-curl -X POST -d 'name=Play Cricket' http://localhost:3000/todos
-curl -X POST -d 'name=HomeWork' http://localhost:3000/todos
+curl -X POST -d 'name=Task-1 &startDate=2020-11-01&endDate=2020-11-01&dateCreated=2020-11-01' http://localhost:3000/todos
 
 */
 router.post("/todos", (req,res) => {
     let newTodo = {
-        name: req.body.name
+        name: req.body.name,
+        startDate:req.body.startDate,
+        endDate:req.body.endDate,
+        dateCreated:req.body.dateCreated,
+        pending:true
     }
     Todos.create(newTodo, (err,newlyCreated)=> {
         if (err) {
@@ -39,7 +113,7 @@ router.post("/todos", (req,res) => {
 });
 
 /* Delete a TODO to the list
-curl -X "DELETE" -d 'name=Play Cricket' http://localhost:3000/todos
+curl -X "DELETE" -d 'name=Codenation Interview ' http://localhost:3000/todos
 */
 router.delete("/todos", (req,res) => {
     let deleteTodo = req.body.name;
@@ -53,7 +127,4 @@ router.delete("/todos", (req,res) => {
         }
     });
 });
-
 module.exports = router;
-
-
