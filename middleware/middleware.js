af38ba8f195099c625804d61ab204e6a598ed9e8
Aman Kumar Kesu <aman.kesu@crio-users.in>
2020-11-23 00:16:12 +0530

    [Updated Routes , Removed Redundant Folders ]

--- middleware/middleware.js
+++
@@ -1,7 +0,0 @@
-const captureDate = (req, res, next) => {
-    res.locals.dateCreated = new Date();
-    console.log(`URL:  ${req.url}, Method:  ${req.method}, Timestamp: ${new Date()}`);
-    next();
-}
-
-module.exports = captureDate;
--- models/todo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const todoSchema = new mongoose.Schema({
-    name: String,
-    startDate: {type : Date},
-    endDate: {type : Date},
-    dateCreated: {type : Date},
-    pending: Boolean
-});
-module.exports = mongoose.model("Todos",todoSchema);
