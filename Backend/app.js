b2c8222bd7a95202e36c115f29718fa61c3b0038
Nabhan Abdulla <nabhan@criodo.com>
2021-01-09 13:05:36 +0530

    Move config to separate files

--- Backend/app.js
+++ Backend/app.js
@@ -1,30 +1,32 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
+// const methodOverride = require("method-override");
 const routes = require("./routes");
 const mongoose = require("mongoose");
 const captureDate = require("./middleware/middleware");
 const cors = require("cors");
+const config = require("./config/config")
 
-mongoose.connect("mongodb://localhost:27017/TODOAPP", {
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  console.log("Connected to MongoDB");
+
+  // Start the Node server
+  app.listen(config.port, function () {
+    console.log(`App is running on port ${process.env.PORT}`);
+  });
 });
 
 app.use(cors());
-app.use(express.json());
 
-app.use(captureDate);
+app.use(express.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
 
-app.use("/", routes);
+// FIXME - See if used 
+app.use(captureDate);
 
-app.listen(3001, function () {
-  console.log("App is running");
-});
+app.use("/", routes);
