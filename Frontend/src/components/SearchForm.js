9c2c6ba7c5b577e84b9a590999dd857904845b4e
Jatin <jatin@criodo.com>
2021-01-15 09:03:24 +0530

    CSS Initial commit

--- Frontend/src/components/SearchForm.js
+++ Frontend/src/components/SearchForm.js
@@ -1,149 +1,170 @@
-import React, {Component} from 'react';
-import { Button } from 'antd';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Button } from "antd";
+import axios from "axios";
 
-import {Link, withRouter } from 'react-router-dom';
-import TodoList from './TodoList';
+import { Link, withRouter } from "react-router-dom";
+import TodoList from "./TodoList";
 
-class SearchForm extends Component{
-    state = {
-      tasks: [],
-    name: '',
-    startDate: '',
-    endDate: ''};
-  
+class SearchForm extends Component {
+  state = {
+    tasks: [],
+    name: "",
+    startDate: "",
+    endDate: ""
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        if(this.state.startDate === '' || this.state.endDate==='') return;
-        
-        let todoData = [];
-        let task = this.state;
-        axios.get(`http://localhost:3001/todos/search`,
-        {
-          params:{
-            startDate:task.startDate,
-            endDate:task.endDate
-          }
-        })
-        .then(res => {
-          for( let i =0 ;i< res.data.length;i++) {
-            todoData.push(res.data[i]);
-          }
-        })
-        .then( ()=>{
-          this.setState({tasks:[]});
-          for( let i =0 ;i< todoData.length;i++) {
-          this.setState({tasks: [...this.state.tasks, todoData[i]]});
-          }
-        });
-        console.log(todoData);
-        this.setState({ 
-          name: '' ,
-          startDate: '',
-          endDate: ''});
-      }
-      handleDelete = (index) => {
-        const newArr = [...this.state.tasks];
-        let pos = -1;
-        for( let i =0 ; i< newArr.length;i++ ){
-          if( newArr[i].id === index){
-            pos = i;
-            break;
-          }
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.startDate === "" || this.state.endDate === "") return;
+
+    let todoData = [];
+    let task = this.state;
+    axios
+      .get(`http://localhost:3001/todos/search`, {
+        params: {
+          startDate: task.startDate,
+          endDate: task.endDate
         }
-        if( pos === -1) return;
-        let task = newArr[pos];
-        console.log(task);
-        axios.delete(`http://localhost:3001/todos`,  { data: task })
-        .then(res => {
-          console.log("In Frontend",res.data);
-        });
-        newArr.splice(pos, 1);
-        this.setState({tasks: newArr});
-      }
-      handleUpdate = (task) => {
-        const newArr = [...this.state.tasks];
-    
-        let pos = -1;
-        for( let i =0 ; i< newArr.length; i++ ){
-          if( newArr[i].id === task.id){
-            pos = i;
-            break;
-          }
+      })
+      .then(res => {
+        for (let i = 0; i < res.data.length; i++) {
+          todoData.push(res.data[i]);
         }
-        if( pos === -1) return;
-        newArr[pos].name = task.name;
-        newArr[pos].startDate = task.startDate;
-        newArr[pos].endDate= task.endDate;
-        let updateTask = newArr[pos];
-        console.log(updateTask);
-        axios.put(`http://localhost:3001/todos`,  { data: updateTask })
-        .then(res => {
-          console.log("Updating In Frontend",res.data);
-        });
-        this.setState({tasks: newArr});
+      })
+      .then(() => {
+        this.setState({ tasks: [] });
+        for (let i = 0; i < todoData.length; i++) {
+          this.setState({ tasks: [...this.state.tasks, todoData[i]] });
+        }
+      });
+    console.log(todoData);
+    this.setState({
+      name: "",
+      startDate: "",
+      endDate: ""
+    });
+  };
+  handleDelete = index => {
+    const newArr = [...this.state.tasks];
+    let pos = -1;
+    for (let i = 0; i < newArr.length; i++) {
+      if (newArr[i].id === index) {
+        pos = i;
+        break;
       }
-      handleInputChange = (event) => {
-        event.preventDefault()
-        this.setState({
-           [ event.target.name]: event.target.value
-        })
-      }
-      performAPICall =  () => {
-    
-        let todoData = [];
-        axios.get(`http://localhost:3001/todos`)
-        .then(res => {
-          for( let i =0 ;i< res.data.length;i++) {
-            todoData.push(res.data[i]);
-          }
-        })
-        .then( ()=>{
-          for( let i =0 ;i< todoData.length;i++) {
-          this.setState({tasks: [...this.state.tasks, todoData[i]]});
-          }
-        });
-      }
-      
-      componentDidMount() {
-         this.performAPICall();
-      }
+    }
+    if (pos === -1) return;
+    let task = newArr[pos];
+    console.log(task);
+    axios.delete(`http://localhost:3001/todos`, { data: task }).then(res => {
+      console.log("In Frontend", res.data);
+    });
+    newArr.splice(pos, 1);
+    this.setState({ tasks: newArr });
+  };
+  handleUpdate = task => {
+    const newArr = [...this.state.tasks];
 
-      render() {
-        const {startDate} = this.state;
-        const { endDate} = this.state;
-        return(
-          <div className="home-container container"> 
-            <Link to="/">
-                <Button className="btn-block" type="primary"  >
-                    Back
-                </Button>
-            </Link> 
+    let pos = -1;
+    for (let i = 0; i < newArr.length; i++) {
+      if (newArr[i].id === task.id) {
+        pos = i;
+        break;
+      }
+    }
+    if (pos === -1) return;
+    newArr[pos].name = task.name;
+    newArr[pos].startDate = task.startDate;
+    newArr[pos].endDate = task.endDate;
+    let updateTask = newArr[pos];
+    console.log(updateTask);
+    axios.put(`http://localhost:3001/todos`, { data: updateTask }).then(res => {
+      console.log("Updating In Frontend", res.data);
+    });
+    this.setState({ tasks: newArr });
+  };
+  handleInputChange = event => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+  performAPICall = () => {
+    let todoData = [];
+    axios
+      .get(`http://localhost:3001/todos`)
+      .then(res => {
+        for (let i = 0; i < res.data.length; i++) {
+          todoData.push(res.data[i]);
+        }
+      })
+      .then(() => {
+        for (let i = 0; i < todoData.length; i++) {
+          this.setState({ tasks: [...this.state.tasks, todoData[i]] });
+        }
+      });
+  };
+
+  componentDidMount() {
+    this.performAPICall();
+  }
+
+  render() {
+    const { startDate } = this.state;
+    const { endDate } = this.state;
+    return (
+      <div className="wrapper">
+        <header className="header">
+          <div className="header-content">
+            <div>
+              <p className="header-text">Crio Notes App</p>
+            </div>
+            <div className="header-action-container">
+              <div>
+                <Link to="/">
+                  <button className="primary-button" block={true}>
+                    All To-Dos
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </header>
+        <div className="main-section">
+          {/* <Link to="/">
+            <button className="primary-button">Back</button>
+          </Link> */}
           <form onSubmit={this.handleSubmit}>
-             <input 
-             type='date'
-             placeholder='Start Date'
-             className='input'
-              name='startDate'
+            <input
+              type="date"
+              placeholder="Start Date"
+              className="input-date"
+              name="startDate"
               value={startDate}
               onChange={this.handleInputChange}
             />
-             <input 
-             type='date'
-             className='input'
-              placeholder='End Date'
-              name='endDate'
+            <input
+              type="date"
+              className="input-date"
+              placeholder="End Date"
+              name="endDate"
               value={endDate}
               onChange={this.handleInputChange}
             />
-            <button class='button1'>Submit</button>
+            <button style={{ padding: "10px 30px" }} className="primary-button">
+              Search
+            </button>
           </form>
-          <TodoList tasks={this.state.tasks} onDelete={this.handleDelete} 
-             onUpdate={this.handleUpdate} />
-         
-          </div> 
-        );
-    }
+          <div style={{ marginTop: "40px" }}>
+            <TodoList
+              tasks={this.state.tasks}
+              onDelete={this.handleDelete}
+              onUpdate={this.handleUpdate}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default withRouter(SearchForm);
--- Frontend/src/components/SubmitForm.js
+++ Frontend/src/components/SubmitForm.js
@@ -1,62 +1,75 @@
-import React from 'react';
+import React from "react";
 
 class SubmitForm extends React.Component {
-    state = { name: '',
-    startDate: '',
-    endDate: ''};
-  
-    handleSubmit = (event) => {
-      event.preventDefault();
-      if(this.state.name === '' || this.state.startDate==='' || this.state.endDate==='') return;
-      this.props.onFormSubmit(this.state);
-      this.setState({ 
-        name: '' ,
-        startDate: '',
-        endDate: ''});
-    }
-    handleInputChange = (event) => {
-      event.preventDefault()
-      this.setState({
-         [ event.target.name]: event.target.value
-      })
-    }
-  
-    render() {
-  
-      const {name} = this.state;
-      const {startDate} = this.state;
-      const { endDate} = this.state;
-      return(
-        <form onSubmit={this.handleSubmit}>
-          <input 
-            type='text'
-            className='input'
-            placeholder='Enter Item'
-            name='name'
+  state = { name: "", startDate: "", endDate: "" };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (
+      this.state.name === "" ||
+      this.state.startDate === "" ||
+      this.state.endDate === ""
+    )
+      return;
+    this.props.onFormSubmit(this.state);
+    this.setState({
+      name: "",
+      startDate: "",
+      endDate: ""
+    });
+  };
+  handleInputChange = event => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  render() {
+    const { name } = this.state;
+    const { startDate } = this.state;
+    const { endDate } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit} style={{ display: "block" }}>
+        <div class="input-container">
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Enter Item"
+            name="name"
             value={name}
             onChange={this.handleInputChange}
           />
-          
-           <input 
-           type='date'
-           placeholder='Start Date'
-           className='input'
-         
-            name='startDate'
+        </div>
+
+        <div class="input-container">
+          <input
+            type="date"
+            placeholder="Start Date"
+            className="input-date"
+            name="startDate"
             value={startDate}
             onChange={this.handleInputChange}
           />
-           <input 
-           type='date'
-           className='input'
-            placeholder='End Date'
-            name='endDate'
+        </div>
+        <div class="input-container">
+          <input
+            type="date"
+            className="input-date"
+            placeholder="End Date"
+            name="endDate"
             value={endDate}
             onChange={this.handleInputChange}
           />
-          <button class='button1'>Submit</button>
-        </form>
-      );
-    }
+        </div>
+        <div style={{ margin: "25px 0px 0px" }}>
+          <button class="primary-button">Add To-Do</button>
+          <button class="secondary-button" onClick={this.props.onClose}>
+            Close
+          </button>
+        </div>
+      </form>
+    );
   }
-  export default SubmitForm;
+}
+export default SubmitForm;
