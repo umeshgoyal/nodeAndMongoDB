cc647810599bbf2152ddc550f34104bdfc8e74cf
Nabhan Abdulla <nabhan@criodo.com>
2021-01-20 17:04:19 +0530

    [Frontend/] Add setup script

---
+++ Frontend/setup.sh
@@ -0,0 +1,15 @@
+# Store workspace public IP to config file
+IP_VAR="$(curl http://checkip.amazonaws.com/)"
+CONFIG='{"workspaceIp": "'"$IP_VAR"'"}'
+echo $CONFIG > src/ipConfig.json
+# CRIO_SOLUTION_AND_STUB_END_MODULE_LOGIN
+
+# Install project dependencies
+rm -rf ~/node_modules
+rm -rf node_modules
+rm -rf ~/.npm 
+rm package-lock.json
+mkdir -p ~/node_modules
+ln -s /home/crio-user/node_modules $PWD/node_modules
+npm install
+npm start
