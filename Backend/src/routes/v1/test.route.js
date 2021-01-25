1563f26cf12482e92e6da7775988d53faf4d3e9d
Nabhan Abdulla <nabhan@criodo.com>
2021-01-25 13:54:46 +0530

    [Backend/] add import statement

--- Backend/src/routes/v1/test.route.js
+++ Backend/src/routes/v1/test.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const captureDateMiddleware = require("../../middleware/middleware");
 
 // router.use(captureDateMiddleware);
 
--- Backend/src/routes/v1/todo.route.js
+++ Backend/src/routes/v1/todo.route.js
@@ -27,7 +27,7 @@ router.get("/", async (req, res) => {
 /**
  * Update an existing TODO
  * curl -v -X PUT http://localhost:8082/v1/todos \
-    -d '{"_id": "<id-value"", "name": "Play tennis","startDate": "2021-01-07","endDate": "2021-01-09"}' \
+    -d '{"_id": "<id-value>", "name": "Play tennis","startDate": "2021-01-07","endDate": "2021-01-09"}' \
     -H 'Content-Type: application/json'
  * 
  * Nb: You'll need to change the "id" value to that of one of your todo items
